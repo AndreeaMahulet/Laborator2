d1232aae1164900662ee374947a700efd9f81bce
Andreea Mahulet <andreea.mahulet@yahoo.com>
2021-10-21 18:15:09 +0300

    lab 4

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,17 +6,36 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Mahulet_Andreea_Lab2.Models;
-
+using Microsoft.EntityFrameworkCore;
+using Mahulet_Andreea_Lab2.Models.LibraryViewModels;
+using Mahulet_Andreea_Lab2.Data;
 namespace Mahulet_Andreea_Lab2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly LibraryContext _context;
+        public HomeController(LibraryContext context)
+        {
+            _context = context;
+        }
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderGroup> data =
+            from order in _context.Orders
+            group order by order.OrderDate into dateGroup
+            select new OrderGroup()
+            {
+                OrderDate = dateGroup.Key,
+                BookCount = dateGroup.Count()
+            };
+            return View(await data.AsNoTracking().ToListAsync());
+        }
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+       /* public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
+        }*/
 
         public IActionResult Index()
         {
